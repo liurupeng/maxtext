97a89b177d88a1868226e82bd2856950cf9419b2
Rupeng Liu <rupliu@google.com>
2024-10-09 23:58:28 +0000

    tem

--- MaxText/maxengine_config.py
+++ MaxText/maxengine_config.py
@@ -44,7 +44,7 @@ def get_server_config(config_str: str, config: Any) -> Type[config_lib.ServerCon
       )
     case "MaxtextDisaggregatedServer":
       server_config = config_lib.ServerConfig(
-        prefill_slice=("v5e-8"),
+        prefill_slices=("v5e-8"),
         generate_slices=("v5e-8"),
         interleaved_slices=(),
         prefill_engine_create_fns=(functools.partial(create_disag_maxengine, config=config)),
